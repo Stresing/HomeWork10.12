102aab196459750efd40cbca155d6eae7672999a
stresing <Stresingrad@gmail.com>
2022-12-10 03:00:11 +0700

    final task 2 and task 3

--- main.py
+++ main.py
@@ -1,12 +1,46 @@
 from decimal import Decimal
+import math
 
-d = str(input("Введите число точности : "))
-n = float(input('Ведите второе число: '))
-nope_int = Decimal(d) % 1
-some_list = list(str(nope_int))
-if nope_int != 0:
-    some_list.remove("0")
-    some_list.remove('.')
-    print(format(n, f'10.{len(some_list)}f'))
-else:
-    print(int(n))
+def Task1():
+    d = str(input("Введите число точности : "))
+    n = float(input('Ведите второе число: '))
+    nope_int = Decimal(d) % 1
+    some_list = list(str(nope_int))
+    if nope_int != 0:
+        some_list.remove("0")
+        some_list.remove('.')
+        print(format(n, f'10.{len(some_list)}f'))
+    else:
+        print(int(n))
+
+def Task2():
+    N = int(input("Введите простое число: "))
+    some_list = []
+    for i in range(2, int(math.sqrt(N)) + 1):
+        while (N % i == 0):
+            some_list.append(i)
+            N //= i
+
+    if (N != 1):
+        some_list.append(N)
+        print(some_list)
+
+
+def Task3():
+    some_list = list(map(int, input("Для заполнение списка, введите числа через пробел: ").split()))
+    some_two_list = []
+    print(some_list)
+    for i in some_list:
+            if i not in some_two_list:
+                some_two_list.append(i)
+    print(some_two_list)
+
+
+task = int(input('Введите номер(1-3) задания: '))
+if task == 1:
+    Task1()
+elif task == 2:
+    Task2()
+elif task == 3:
+    Task3()
+
